5fa7b49c28c6254edd36fbc2e783e43faa0ad2f9
dewimawaddah <dewimawaddahwarrohmah@gmail.com>
2023-09-18 00:37:15 +0700

    done impelementasi POM Register

--- cypress.config.js
+++ cypress.config.js
@@ -3,9 +3,8 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     env: {
-      demowebshop_url: "https://demowebshop.tricentis.com/",
+      demowebshop_url: "https://demowebshop.tricentis.com",
     },
-    testIsolation: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/demowebshop/register.cy.js
+++ cypress/e2e/demowebshop/register.cy.js
@@ -1,40 +1,43 @@
+import RegisterPage from "../../support/pageObject/registerPage.cy";
+
 describe("template spec", () => {
   beforeEach(() => {
     cy.visit(Cypress.env("demowebshop_url"));
   });
 
-  it.only("success register", () => {
+  it.only("success register with POM", () => {
+    const register = new RegisterPage();
+    register.visitRegisterPage();
     cy.fixture("success_register.json").then((user) => {
-      const datauser = user[0];
-      cy.get(".ico-register").click();
-      cy.url().should("include", "/register");
-      cy.get("#gender-female").click();
-      cy.input("#FirstName", datauser.firstname);
-      cy.input("#LastName", datauser.lastname);
-      cy.input("#Email", datauser.email);
-      cy.input("#Password", datauser.password);
-      cy.input("#ConfirmPassword", datauser.confirmPassword);
-      cy.clickbtn("#register-button");
-      cy.validateText(".result", "Your registration completed");
-      cy.wait(1000);
-      cy.get(".ico-logout").click();
+      register.assertURL();
+      register.selectGender();
+      register.inputFirstname(user.firstname);
+      register.inputLastname(user.lastname);
+      register.inputEmail(user.email);
+      register.inputPassword(user.password);
+      register.inputConfirmPassword(user.confirmPassword);
+      register.clickRegisterButton();
+      register.assertTextSuccess();
     });
   });
 
-  it("failed register", () => {
+  it("failed register with POM", () => {
+    const register = new RegisterPage();
+    register.visitRegisterPage();
     cy.fixture("failed_register.json").then((user) => {
-      cy.get(".ico-register").click();
       user.failed_regis.forEach((datauser) => {
-        cy.url().should("include", "/register");
-        cy.get("#gender-female").click();
-        cy.input("#FirstName", datauser.firstname);
-        cy.input("#LastName", datauser.lastname);
-        cy.input("#Email", datauser.email);
-        cy.input("#Password", datauser.password);
-        cy.input("#ConfirmPassword", datauser.confirmPassword);
-        cy.clickbtn("#register-button");
+        register.selectGender();
+        register.inputFirstname(datauser.firstname);
+        register.inputLastname(datauser.lastname);
+        register.inputEmail(datauser.email);
+        register.inputPassword(datauser.password);
+        register.inputConfirmPassword(datauser.confirmPassword);
+        register.clickRegisterButton();
         cy.wait(1000);
-        cy.validateText(datauser.errorLocator, datauser.errorMessage);
+        register.assertRegisterFailed(
+          datauser.errorLocator,
+          datauser.errorMessage
+        );
       });
     });
   });
